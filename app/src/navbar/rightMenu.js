ecccc390cc0c5995ce67fb8c7d2c90404af01739
puwaned <puwaneda@gmail.com>
2020-03-02 19:22:05 +0700

    add licence

--- app/src/navbar/rightMenu.js
+++ app/src/navbar/rightMenu.js
@@ -1,48 +1,56 @@
 import React, { Component } from "react";
 import { Menu, Icon } from "antd";
+import Licence from "../components/Home/Licence";
 const { SubMenu } = Menu;
 class RightMenu extends Component {
+  state = { visible: false };
   render() {
+    const handleModal = e => {
+      this.setState({ visible: e });
+    };
     return (
-      <Menu mode="horizontal">
-        <SubMenu
-          title={
-            <span className="submenu-title-wrapper">
-              <Icon type="question-circle" />
-              About
-            </span>
-          }
-        >
-          <Menu.ItemGroup title="About">
-            <Menu.Item key="setting:1">
-              <Icon type="file-unknown" />
-              How to use It!
-            </Menu.Item>
-          </Menu.ItemGroup>
-          <Menu.ItemGroup title="Contact us">
-            <Menu.Item key="setting:3">
-              <a
-                href="https://web.facebook.com/puwanedz"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Icon type="facebook" />
-                facebook
-              </a>
-            </Menu.Item>
-            <Menu.Item key="setting:4">
-              <a
-                href="https://github.com/puwaned"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Icon type="github" />
-                github
-              </a>
-            </Menu.Item>
-          </Menu.ItemGroup>
-        </SubMenu>
-      </Menu>
+      <div>
+        <Licence visible={this.state.visible} parentCallback={handleModal} />
+        <Menu mode="horizontal">
+          <SubMenu
+            title={
+              <span className="submenu-title-wrapper">
+                <Icon type="question-circle" />
+                About
+              </span>
+            }
+          >
+            <Menu.ItemGroup title="About">
+              <Menu.Item key="setting:1" onClick={() => handleModal(true)}>
+                <Icon type="file-unknown" />
+                Disclaimer
+              </Menu.Item>
+            </Menu.ItemGroup>
+            <Menu.ItemGroup title="Contact us">
+              <Menu.Item key="setting:3">
+                <a
+                  href="https://web.facebook.com/puwanedz"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Icon type="facebook" />
+                  facebook
+                </a>
+              </Menu.Item>
+              <Menu.Item key="setting:4">
+                <a
+                  href="https://github.com/puwaned"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Icon type="github" />
+                  github
+                </a>
+              </Menu.Item>
+            </Menu.ItemGroup>
+          </SubMenu>
+        </Menu>
+      </div>
     );
   }
 }
